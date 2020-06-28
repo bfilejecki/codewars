0643cff40461bb8a2041d61022e7487c679eb7ff
Bartosz Filejecki <filus1025@gmail.com>
2020-06-29 00:16:51 +0200

    pig latin WIP

---
+++ 5k-simplePigLatin-520b9d2ad5c005041100000f/solution.py
@@ -0,0 +1,19 @@
+import re
+
+def pig_it(text):
+    words = text.split(' ')
+    pigged_words = []
+    for word in words:
+        rewriten_word = word
+        stripped_word_match = re.match(r'\w*', word)
+        if stripped_word_match is not None:
+            stripped_word = word[stripped_word_match.start():stripped_word_match.end()]
+            pigged_word =  stripped_word[0] + 'ay' if len(stripped_word) < 2 else stripped_word[1:] + stripped_word[0] + 'ay'
+            rewriten_word = pigged_word
+            
+        pigged_words.append(rewriten_word)
+    return ' '.join(pigged_words)
+
+print(pig_it('Pig! latin, cool.'))
+        
+        
