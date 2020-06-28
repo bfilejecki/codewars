33c1b584057b0c94da4cffaa73273ac2e5c42b24
Bartosz Filejecki <filus1025@gmail.com>
2020-06-28 11:42:41 +0200

    Add better solution for areTheyTheSame

---
+++ 5k-weightForWeight-55c6126177c9441a570000cc/solution.py
@@ -0,0 +1,6 @@
+def order_weight(string):
+    numbers = string.split(' ')
+    numbers_weights = {x:}
+
+def digitize(num):
+    
--- 6k-AreTheyTheSame-550498447451fbbd7600041c/solution.py
+++ 6k-AreTheyTheSame-550498447451fbbd7600041c/solution.py
@@ -1,4 +1,4 @@
-def comp(array1, array2):
+def comp_v1(array1, array2):
     if (array1 is None and array2 is not None) or (array1 is not None and array2 is None):
         return False
     if len(array1) != len(array2):
@@ -13,4 +13,8 @@ def comp(array1, array2):
 a1 = [121, 144, 19, 161, 19, 144, 19, 11]
 a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
 
-print(comp(a1,a2))
+
+def comp_v2(array1, array2):
+    return False if None in (array1,array2) else sorted([x**2 for x in array1]) == sorted(array2)
+
+print(comp_v2(a1,a2))
