4e4a709a3ebfa9c61eac4701d4f17e51fbe3fbe3
Bartosz Filejecki <filus1025@gmail.com>
2020-07-13 16:14:56 +0200

    Add solution for directionsReduction

--- 4k-mostFrequentWords-51e056fe544cf36c410000fb/solution.py
+++ 4k-mostFrequentWords-51e056fe544cf36c410000fb/solution.py
@@ -1,10 +1,16 @@
+### IT DOESN"T WORK
+
 import collections
+import re
 
 def top_3_words(text):
+    word_regex = re.compile(r'^[\'A-Za-z]+$')
     words_counter = collections.Counter()
     for word in text.split(' '):
-        sanitized_word = word.lower().strip(' ,./;:\'_-[{]}()*&^%$#@!')
-        if sanitized_word:
+        lower_word = word.lower()
+        sanitized_word = lower_word.strip(',.!@#$%^&*()-={[]}:;\\/<>~`\'')
+
+        if sanitized_word and word_regex.fullmatch(sanitized_word):
             words_counter[sanitized_word] = words_counter[sanitized_word] + 1
 
     return [k for k,v in words_counter.most_common(3)]
---
+++ 5k-directionsReduction-550f22f4d758534c1100025a/solution.py
@@ -0,0 +1,19 @@
+import re
+
+def dirReduc(arr):
+    directions_chain = ' '.join(arr)
+    simplified_directions = simplify_string(directions_chain)
+    return [x for x in simplified_directions.split(' ') if x]
+
+
+def simplify_string(string):
+    directions_pattern = re.compile(r'(NORTH\ +SOUTH)|(SOUTH\ +NORTH)|(EAST\ +WEST)|(WEST\ +EAST)')
+    simplified_string = directions_pattern.sub('', string)
+    if simplified_string == string:
+        return simplified_string
+    return simplify_string(simplified_string)
+
+a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+print(dirReduc(a))
+u=["NORTH", "WEST", "SOUTH", "EAST"]
+print(dirReduc(u))
